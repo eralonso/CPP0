8f04f34f049d1608b83b71d692d6297a0b1bcb02
Éric Alonso Martínez <113835039+eralonso@users.noreply.github.com>
2023-07-01 12:35:51 +0200

    CPP0 - [ex00/ex01/ex02 - Makefile messages update], [ex01 - Print allowed option in init of the program]

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -6,19 +6,25 @@
 /*   By: eralonso <eralonso@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/28 15:26:59 by eralonso          #+#    #+#             */
-/*   Updated: 2023/06/30 18:32:38 by eralonso         ###   ########.fr       */
+/*   Updated: 2023/07/01 12:27:45 by eralonso         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"instructions.hpp"
 
-int	main(void)
+static void	print_options( void )
+{
+	std::cout << "Allowed options:\n *   ADD\n *   SEARCH\n *   EXIT" << std::endl;	
+}
+
+int	main( void )
 {
 	PhoneBook	phonebook;
 	std::string	input;
 	bool		play;
 
 	play = true;
+	print_options();
 	while (play)
 	{
 		input = "a";
@@ -31,7 +37,7 @@ int	main(void)
 		else
 		{
 			std::cout << "Invalid option" << std::endl;
-			std::cout << "Allowed options:\n *   ADD\n *   SEARCH\n *   EXIT" << std::endl;
+			print_options();
 		}
 	}
 	return (0);
